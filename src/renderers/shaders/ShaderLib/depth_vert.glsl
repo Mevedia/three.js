e6f61e0333f5f9808147705bcab03bd3400b7073
Fyrestar <info@mevedia.de>
2017-09-05 19:48:49 +0200

    Update depth_vert.glsl
    Fix logarithmicDepthBuffer with shadows

--- src/renderers/shaders/ShaderLib/depth_vert.glsl
+++ src/renderers/shaders/ShaderLib/depth_vert.glsl
@@ -3,8 +3,15 @@
 #include <displacementmap_pars_vertex>
 #include <morphtarget_pars_vertex>
 #include <skinning_pars_vertex>
-#include <logdepthbuf_pars_vertex>
+
+#ifndef USE_SHADOWMAP
+
+	#include <logdepthbuf_pars_vertex>
+
+#endif
+
 #include <clipping_planes_pars_vertex>
+
 
 void main() {
 
@@ -12,12 +19,21 @@ void main() {
 
 	#include <skinbase_vertex>
 
+	
 	#include <begin_vertex>
 	#include <displacementmap_vertex>
 	#include <morphtarget_vertex>
 	#include <skinning_vertex>
 	#include <project_vertex>
-	#include <logdepthbuf_vertex>
+	
+	#ifndef USE_SHADOWMAP
+	
+		#include <logdepthbuf_vertex>
+	
+	#endif
+	
 	#include <clipping_planes_vertex>
+	
+	
 
 }
